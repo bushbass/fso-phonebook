0f552306ed25034e9eabdd48771c036f610bf252
Alex Nielsen <alex@bigal.org>
2024-03-27 18:28:53 -0400

    start at 3.9

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Error from './components/Error'
 const App = () => {
 
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    // { name: 'Arto Hellas', number: '040-123456', id: 1 },
     // { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
     // { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
     // { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
@@ -56,7 +56,7 @@ const App = () => {
   const createPerson = (person) => {
     personsService.create(person)
       .then(response => {
-        setPersons(persons.concat(response.data))
+        setPersons(response.data)
         setMessage(
           `${person.name} was created`
         )
